f776f740c254a237f8cfef9b1700bd47e62f11e0
Spusya <72402685+Spusya@users.noreply.github.com>
2021-05-21 17:25:00 +0300

    Update ArtifalityPotions.java

--- src/main/java/artifality/item/ArtifalityPotions.java
+++ src/main/java/artifality/item/ArtifalityPotions.java
@@ -10,9 +10,11 @@ import net.minecraft.util.registry.Registry;
 public class ArtifalityPotions {
 
     public static final Potion REBOUND = new Potion(new StatusEffectInstance(ArtifalityEffects.REBOUND, 3600));
+    public static final Potion LONG_REBOUND = new Potion("rebound",new StatusEffectInstance(ArtifalityEffects.REBOUND, 9600));
 
     public static void register() {
 
         Registry.register(Registry.POTION, new Identifier(ArtifalityMod.MODID, "rebound"), REBOUND);
+        Registry.register(Registry.POTION, new Identifier(ArtifalityMod.MODID, "long_rebound"), LONG_REBOUND);
     }
 }
